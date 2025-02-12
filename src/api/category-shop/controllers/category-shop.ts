24e3e746ac0ae165f6c10f4f7d3add719f323b04
rizal212 <zalnugroho1@gmail.com>
2025-02-12 11:05:59 +0700

    add shop

---
+++ src/api/category-shop/controllers/category-shop.ts
@@ -0,0 +1,7 @@
+/**
+ * category-shop controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::category-shop.category-shop');
---
+++ src/api/category-shop/routes/category-shop.ts
@@ -0,0 +1,7 @@
+/**
+ * category-shop router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::category-shop.category-shop');
